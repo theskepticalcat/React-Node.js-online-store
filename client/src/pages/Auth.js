1bce079a4a2066ce4b4ca72d6d3a6c05ceb312af
theskepticalcat <lizaveta_alex@mail.ru>
2021-10-21 20:25:47 +0800

    Added autorization

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Button, Card, Container, Form, Row } from 'react-bootstrap';
 import '../styles/auth.scss';
-import {REGISTRATION_ROUTE} from '../utils/consts';
+import {LOGIN_ROUTE, REGISTRATION_ROUTE} from '../utils/consts';
 import { NavLink, useLocation } from 'react-router-dom';
 
 
 function Auth() {
+    const location = useLocation()  //для отрисовки страницы в зависимости от ссылки
+    console.log(location);  //локация страницы, на кот.находимся
+    const isLogin = location.pathname === LOGIN_ROUTE; 
+    
     return (
         <Container className='auth'>
             <Card className='auth__card'>
-                <h2 className='auth__card-title'>Authorization</h2>
+                <h2 className='auth__card-title'>{isLogin ? 'Authorization' : 'Registration'}</h2>
 
                 <Form className='auth__form'>
                     <Form.Control 
@@ -22,12 +26,18 @@ function Auth() {
                     </Form.Control>
 
                     <Row className='auth__footer'>
-                        <div className='auth__footer-registration'>
-                            <NavLink to={REGISTRATION_ROUTE}>Регистрация</NavLink>
-                        </div>
-                        <Button variant='outline-success' className='auth__footer-btn'>
-                            Войти
-                        </Button>
+                        {isLogin ?
+                            <div className='auth__footer-registration'>
+                                <NavLink to={REGISTRATION_ROUTE}>Регистрация</NavLink>
+                            </div>
+                            :
+                            <div className='auth__footer-registration'>
+                                <NavLink to={LOGIN_ROUTE}>Войти</NavLink>
+                            </div>
+                        }
+                            <Button variant='outline-success' className='auth__footer-btn'>
+                                {isLogin ? 'Войти' : 'Регистрация'}
+                            </Button>
                     </Row>
                 </Form>
             </Card>
